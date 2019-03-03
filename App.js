7a74fe7f9d5ce05fc4954efb21f6d2c3924a1429
Abdrew Rady <andrew.arsoftware@gmail.com>
2019-03-03 13:06:17 -0700

    grabs data from api

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true }
+  }
+
+  componentDidMount() {
+    return fetch('https://guarded-earth-10858.herokuapp.com/rings')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson)
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson
+        }, function() {
+
+        });
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    if(!this.state.dataSource.length) {
+      return(
+        <Text>No Rings are listed</Text>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+      <Text>Tournaments</Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
